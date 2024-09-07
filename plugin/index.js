ef01943eba93dd9aeac847d9c785bedf72ecf649
Patrick McLaughlin <me@patmclaughl.in>
2024-09-07 15:27:24 -0400

    feat: implement all plugins

--- plugin/index.js
+++ plugin/index.js
@@ -1,19 +1,5 @@
 import { execSync } from "child_process";
 
-export async function analyzeCommits(pluginConfig, context) {
-  const { path, originalPlugin } = pluginConfig;
-
-  const filteredCommits = filterCommits(context.commits, path);
-
-  const filteredContext = {
-    ...context,
-    commits: filteredCommits,
-  };
-
-  const plugin = await import(originalPlugin);
-  return plugin.analyzeCommits(pluginConfig, filteredContext);
-}
-
 function filterCommits(commits, path) {
   return commits.filter((commit) => {
     const changedFiles = getChangedFiles(commit.hash);
@@ -36,3 +22,36 @@ function getChangedFiles(commitHash) {
     return [];
   }
 }
+
+function makePlugin(name) {
+  return async function (pluginConfig, context) {
+    const { path, originalPlugin, ...remainingConfig } = pluginConfig;
+
+    const originalPluginName =
+      typeof originalPlugin === "string" ? originalPlugin : originalPlugin[0];
+    const originalPluginConfig = Array.isArray(originalPlugin)
+      ? { ...remainingConfig, ...originalPlugin[1] }
+      : remainingConfig;
+
+    const plugin = await import(originalPluginName);
+    if (!name in plugin) {
+      return;
+    }
+
+    const filteredContext = {
+      ...context,
+      commits: filterCommits(context.commits, path),
+    };
+    return plugin[name](originalPluginConfig, filteredContext);
+  };
+}
+
+export const verifyConditions = makePlugin("verifyConditions");
+export const analyzeCommits = makePlugin("analyzeCommits");
+export const generateNotes = makePlugin("generateNotes");
+export const verifyRelease = makePlugin("verifyRelease");
+export const prepare = makePlugin("prepare");
+export const publish = makePlugin("publish");
+export const addChannel = makePlugin("addChannel");
+export const success = makePlugin("success");
+export const fail = makePlugin("fail");
