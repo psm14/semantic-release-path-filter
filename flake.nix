03a3dbb3466093b38bcc00f9fd4d01f3eec462e1
Patrick McLaughlin <me@patmclaughl.in>
2025-11-08 14:35:30 -0500

    chore: update to node 24

--- flake.nix
+++ flake.nix
@@ -1,21 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            nodePackages.npm
+          packages = [
+            pkgs.nodejs_24
           ];
         };
-      }
-    );
+      });
 }
